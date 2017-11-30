060491c554afb1cd32e23ec35887a11bc78c1214
lehronn <lehronn@gmail.com>
2017-11-30 11:52:11 +0100

    finish 20.4

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/index';
+import routes from './routes';
+// import styles from './country.css';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
 
 render(
-    <Provider store={store}>
-        <div>
-          <h1>Project Header</h1>
-          <DevTools />
-        </div>
+  <Provider store={store}>
+    <div>
+      <Router history={hashHistory} routes={routes}>
+      </Router>
+      <DevTools />
+    </div>
 
-    </Provider>,
-    document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 
-store.dispatch(getCountries()); //wywołanie akcji za pomocą dyspozytora.
+
 
 //provider może dostać tylko jeden element, dlatego jest w divie.
+
+// Komponent <Router> może przyjmować opcjonalny
+// parametr routes. Dzięki temu, możemy wydzielić
+// wszystkie tworzone route'y do osobnego pliku, który
+// zazwyczaj nazywa się po prostu routes.js. Jest to
+// bardzo dobra praktyka.
